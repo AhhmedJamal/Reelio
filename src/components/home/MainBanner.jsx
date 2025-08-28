95cb26956b2dd8250d8d836348caa5761a145510
Ahmed Jamal <ahhmed.jamal@gmail.com>
2025-08-28 16:52:21 +0300

    update: site

---
+++ src/components/home/MainBanner.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useFetchTMDB } from "../../hooks/useFetchTMDB";
+
+const MainBanner = () => {
+  const {
+    data: movies,
+    loading,
+    error,
+  } = useFetchTMDB(import.meta.env.VITE_URL_MOVIE);
+
+  if (loading) return <p>Loading…</p>;
+  if (error) return <p>Error: {error}</p>;
+  return (
+    <div>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 place-items-center">
+        {movies.map((m) => (
+          <li key={m.id}>
+            {m.title} {console.log(m)}
+            <img
+              src={`https://image.tmdb.org/t/p/original${m.poster_path}`}
+              alt=""
+            />
+            <img
+              src={`https://image.tmdb.org/t/p/original${m.backdrop_path}`}
+              alt=""
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MainBanner;
