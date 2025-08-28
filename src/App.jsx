cce9771c0904739ca5e354d61eb5d2cd02964075
Ahmed Jamal <81833844+AhhmedJamal@users.noreply.github.com>
2025-08-28 08:10:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,12 @@ export default function App() {
       {movies.map((m) => (
         <li key={m.id}>
           {m.title} {console.log(m)}
-          <img src={`https://image.tmdb.org/t/p/w500${m.poster_path}`} alt="" />
           <img
-            src={`https://image.tmdb.org/t/p/w500${m.backdrop_path}`}
+            src={`https://image.tmdb.org/t/p/original${m.poster_path}`}
+            alt=""
+          />
+          <img
+            src={`https://image.tmdb.org/t/p/original${m.backdrop_path}`}
             alt=""
           />
         </li>
